800638b2a711387e817ebf66e50fa7125251ed0a
MeirambekNarxoz <meirambek.alikulov@narxoz.kz>
2026-04-06 15:55:54 +0500

    first commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,13 +7,16 @@ import (
 	"interaction-service/internal/repository"
 	"interaction-service/internal/routes"
 	"interaction-service/internal/services"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	cfg := config.LoadConfig()
 	db := database.InitDB(cfg)
+
 	likeRepo := repository.NewLikeRepository(db)
 	bookmarkRepo := repository.NewBookmarkRepository(db)
 
@@ -24,6 +27,13 @@ func main() {
 	bookmarkHandler := http.NewBookmarkHandler(bookmarkService)
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:    []string{"Origin", "Content-Type", "Authorization"},
+		MaxAge:          12 * time.Hour,
+	}))
 
 	routes.RegisterInteractionRoutes(
 		r,
